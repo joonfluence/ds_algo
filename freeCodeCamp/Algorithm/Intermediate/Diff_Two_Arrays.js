bc31b22ec176821f01bc9cf9a30ca08dd4cfacf1
joonho lee <goodfluence@naver.com>
2021-10-18 12:53:15 +0900

    Intermediate Algorithm - Diff Two Array

--- freeCodeCamp/Algorithm/Intermediate/Diff_Two_Arrays.js
+++ freeCodeCamp/Algorithm/Intermediate/Diff_Two_Arrays.js
@@ -1,19 +1,16 @@
 function diffArray(arr1, arr2) {
   var newArr = [];
 
-  if (arr1.length < arr2.length) {
-    arr2.map((item) => {
-      if (!arr1.includes(item)) {
-        newArr.push(item);
-      }
-    });
-  } else {
-    arr1.map((item) => {
-      if (!arr2.includes(item)) {
-        newArr.push(item);
-      }
-    });
-  }
+  arr2.map((item) => {
+    if (!arr1.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  arr1.map((item) => {
+    if (!arr2.includes(item)) {
+      newArr.push(item);
+    }
+  });
 
   console.log(`file name [Diff_Two_Arrays.js], line -> 9 >>>>> `, newArr);
   return newArr;

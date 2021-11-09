27e42440afe01793f40fbe3a36638c83ddf8079b
joonho lee <goodfluence@naver.com>
2021-11-09 12:51:07 +0900

    Functional Programming 2)

--- freeCodeCamp/functionalProgramming/second.js
+++ freeCodeCamp/functionalProgramming/second.js
@@ -1,12 +1,15 @@
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => "greenTea";
+const prepareGreenTea = () => "greenTea";
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => "blackTea";
 
 /*
 Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+const getTea = (prepareTea, numOfCups) => {
   const teaCups = [];
 
   for (let cups = 1; cups <= numOfCups; cups += 1) {
@@ -17,5 +20,8 @@ const getTea = (numOfCups) => {
 };
 
 // Only change code below this line
-const tea4TeamFCC = null;
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
+
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);

9f398764dd563f99bd697283d524c8bc6a220709
joonho lee <goodfluence@naver.com>
2021-11-19 00:00:10 +0900

    Functional Programming 9-11

---
+++ freeCodeCamp/functionalProgramming/nineth.js
@@ -0,0 +1,20 @@
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function (callback) {
+  // Only change code below this line
+  const newArray = [];
+  s.forEach((item) => {
+    if (callback(item)) {
+      newArray.push(item);
+    }
+  });
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myFilter(function (item) {
+  return item % 2 === 1;
+});
+
+console.log(`file name [nineth.js], line -> 16 >>>>> `, new_s);

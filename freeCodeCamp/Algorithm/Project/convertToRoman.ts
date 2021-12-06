5c9bb609eb9987721adde9e4350526d715dc8b0b
joonho lee <goodfluence@naver.com>
2021-12-06 22:49:00 +0900

    DS Project 2) convert To Roman

--- freeCodeCamp/Algorithm/Project/convertToRoman.ts
+++ freeCodeCamp/Algorithm/Project/convertToRoman.ts
@@ -1,48 +1,66 @@
-function numToArr(num: number) {
-  return Array.from(String(num), (data) => Number(data));
+function ArrayforLoop(arr: string[], tryNum: number, creature: string) {
+  for (let index = 0; index < tryNum; index++) {
+    arr.push(`${creature}`);
+  }
 }
 
 function convertToRoman(num: number) {
-  const numArr = numToArr(num);
-  // const arrLength = numArr.length;
+  const arr: string[] = [];
+  let tempNum = num;
 
-  // numArr.map((item, index) => {
-  //   if (index === 0) {
-
-  //   } else if (index === 1) {
-  //   } else if (index === 2) {
-  //   } else if (index === 3) {
-  //   } else {
-  //     console.log("I really don't really know, sorry man");
-  //   }
-  // });
-  // let divider = 1;
-  // while (num / divider > 0) {
-  //   switch (num / divider) {
-  //     case 1:
-  //       return "I";
-  //     case 2:
-  //       return "II";
-  //     case 3:
-  //       return "III";
-  //     case 4:
-  //       return "IV";
-  //     case 5:
-  //       return "V";
-  //     case 6:
-  //       return "VI";
-  //     case 7:
-  //       return "VII";
-  //     case 8:
-  //       return "VIII";
-  //     case 9:
-  //       return "IX";
-  //     default:
-  //       break;
-  //   }
-  //   divider *= 10;
-  // }
-  return num;
+  while (tempNum >= 1) {
+    if (tempNum >= 1000) {
+      const tryNumber = Math.floor(tempNum / 1000);
+      ArrayforLoop(arr, tryNumber, "M");
+      tempNum %= 1000;
+    } else if (tempNum >= 500) {
+      if (Math.floor((tempNum / 100) % 5) === 4) {
+        arr.push("CM");
+        tempNum %= 900;
+      } else {
+        ArrayforLoop(arr, Math.floor(tempNum / 500), "D");
+        tempNum %= 500;
+      }
+    } else if (tempNum >= 100) {
+      if (Math.floor(tempNum / 100) === 4) {
+        arr.push("CD");
+      } else {
+        ArrayforLoop(arr, Math.floor(tempNum / 100), "C");
+      }
+      tempNum %= 100;
+    } else if (tempNum >= 50) {
+      if (Math.floor((tempNum / 10) % 5) === 4) {
+        arr.push("XC");
+        tempNum %= 90;
+      } else {
+        ArrayforLoop(arr, Math.floor(tempNum / 50), "L");
+        tempNum %= 50;
+      }
+    } else if (tempNum >= 10) {
+      if (Math.floor(tempNum / 10) === 4) {
+        arr.push("XL");
+      } else {
+        ArrayforLoop(arr, Math.floor(tempNum / 10), "X");
+      }
+      tempNum %= 10;
+    } else if (tempNum >= 5) {
+      if (Math.floor(tempNum % 5) === 4) {
+        arr.push("IX");
+        break;
+      } else {
+        ArrayforLoop(arr, Math.floor(tempNum / 5), "V");
+      }
+      tempNum %= 5;
+    } else {
+      if (tempNum === 4) {
+        arr.push("IV");
+      } else {
+        ArrayforLoop(arr, tempNum / 1, "I");
+      }
+      break;
+    }
+  }
+  return arr.join("");
 }
 
-convertToRoman(36);
+convertToRoman(99);
